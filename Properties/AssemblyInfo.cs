e95bfed62867902e7d887b80f18c0d90ca66b96a
ken3613 <frsy@vip.qq.com>
2018-06-26 00:36:58 +0800

    我再也不乱改版本号了

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows;
 [assembly: AssemblyDescription("倒计时~姬~")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nona")]
-[assembly: AssemblyProduct("DeskTop")]
+[assembly: AssemblyProduct("Countdown-Hime")]
 [assembly: AssemblyCopyright("©2018 Nona&2018届高三6班")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2018.6.6666")]
-[assembly: AssemblyFileVersion("1.2018.6.6666")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
